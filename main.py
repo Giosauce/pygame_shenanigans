790b9677819b5af5974b11c9824d6ef9f46bdd6c
Giosauce <g.hernadez94@gmail.com>
2025-05-12 18:17:57 -0600

    Implement group system for game objects and fix player movement

--- main.py
+++ main.py
@@ -12,12 +12,24 @@ def main():
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill((0, 0, 0))
+        for obj in drawable:
+            obj.draw(screen)
+
         dt = clock.tick(60) / 1000
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
         pygame.display.flip()
